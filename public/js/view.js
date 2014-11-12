1a7a40e5f0bf9c83c70726731ca9fb9465f8c7db
Julian Horvat <julian.m.horvat@gmail.com>
2014-11-12 14:58:54 -0500

    dynamic navbar!

--- public/js/view.js
+++ public/js/view.js
@@ -12,10 +12,24 @@ var View = Backbone.View.extend({
 
 boneboiler.views.nav = View.extend({
     initialize: function() {
+        // Logged in
+        if (boneboiler.user) {
+            this.menu = [
+                "<li><a href=\"#\">Home</a></li>",
+                "<li><a href=\"#\">Harvesting Events</a></li>",
+                "<li><a href=\"#\">Account</a></li>",
+            ];
+        } else {
+            this.menu = [
+                "<li><a href=\"#\">Login</a></li>"
+            ];
+        }
         this.render();
     },
     render: function() {
         this.$el.html(_.template($('#navTPL').html()));
+
+        this.$el.find("#actions").html(this.menu.join(""))
     }
 })
 
